0a65c5b8f33d67b70d86ce6a21c9621c559d9dc4
Arwin-Fabro <flarwingo@gmail.com>
2024-01-10 10:27:09 +0800

    Unity Created by Arwin

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody PlayerRigidbody;
+    public float speed;
+    private int Coins;
+    private int TotalCoins;
+    public Text coinText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TotalCoins = GameObject.FindGameObjectsWithTag("Coins").Length;
+    }
+
+    void Update()
+    {
+        coinText.text = "Coins :" + Coins;
+    }
+
+    void FixedUpdate()
+    {
+        float MoveHorizontal = Input.GetAxis("Horizontal");
+        float MoveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(MoveHorizontal, 0, MoveVertical);
+        transform.Translate(movement * Time.deltaTime * speed);
+    }
+
+    public void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Coins")
+        {
+            Coins++;
+            Destroy(other.gameObject);
+            if (Coins == TotalCoins)
+            {
+                SceneManager.LoadScene("Level2");
+            }
+        }
+        if (other.gameObject.tag == "Obstacle")
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+}
